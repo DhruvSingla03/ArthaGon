6ad4d5866b15408beca58221e24386c009325e81
Dhruv Singla <singladhruv18@gmail.com>
2023-02-05 09:15:07 +0530

    landing done

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,8 @@ import React, { useContext } from "react";
 import Image from 'next/image'
 import Link from "next/link";
 import { UserContext } from "@/context/role";
-// import illustration from "../public/assets/undraw_Finance_re_gnv2.png"
+import illustration from "../public/assets/undraw_Finance_re_gnv2-removebg-preview.png"
+import Cookies from "js-cookie";
 function LandingPage() {
   const { data: session } = useSession();
   const {type, setType} = useContext(UserContext)
@@ -20,40 +21,46 @@ function LandingPage() {
           {!session?.user && (
             <button
               onClick={() => signIn()}
-              className="bg-white hover:bg-yellow-100 text-black font-semibold py-2 px-4 border border-black hover:border-transparent rounded">
-              SignUp/LogIn
+              className="bg-white hover:bg-yellow-100 text-black font-semibold py-2 px-4 border border-black hover:border-transparent rounded my-3 mx-2">
+              LogIn
             </button>
           )}
           {session?.user && (
             <button
               onClick={() => signOut()}
-              className="bg-white hover:bg-yellow-100 text-black font-semibold py-2 px-4 border border-black hover:border-transparent rounded"
+              className="bg-white hover:bg-yellow-100 text-black font-semibold py-2 px-4 border border-black hover:border-transparent rounded my-3 mx-2"
             >
               Sign Out
             </button>
           )}
         </div>
 
-        <div className="flex flex-col-reverse  md:flex-row md:flex mt-24 md:justify-between p-7">
-          <Container className="border-b-2 bg-yellow-100 w-full md:w-1/2 p-4">
-            <div className="text-5xl text-black font-bold  py-4 px-3">Welcome to Arthagon</div>
-            <div className="mt-5 p-5">
-            "Empower Your Business with Innovative Solutions"<br></br><br></br><br></br>
-
-Welcome to our cutting-edge business platform, where we strive to bring the latest technology to help businesses run their operations smoothly and efficiently. Our unique blend of Next.js, Next Auth, TailwindCSS, Prisma ORM, and PostgreSQL powered by Supabase promises to deliver an experience that is both seamless and powerful. With our platform, businesses can tackle their day-to-day operations with ease, making the most of their time and resources. Whether you're looking to manage financial transactions, view account balances, or optimize your financial performance, we've got you covered. So join us today and take your business to the next level!
-            </div>
-
-            <div className="flex">
-              <div className="w-1/2 p-3 h-24">
-                <Button content="Are you a customer?"/>
+        <div className="flex">
+          <div className="flex flex-col-reverse w-1/2 md:flex-row md:flex mt-24 md:justify-between p-7">
+            <Container className="border-b-2 bg-yellow-100 w-full md: p-4">
+              <div className="text-5xl text-black font-bold  py-4 px-3">Welcome to Arthagon</div>
+              <div className="mt-5 p-5">
+              "Empower Your Business with Innovative Solutions"<br></br><br></br><br></br>
+          
+          Welcome to our cutting-edge business platform, where we strive to bring the latest technology to help businesses run their operations smoothly and efficiently. Our unique blend of Next.js, Next Auth, TailwindCSS, Prisma ORM, and PostgreSQL powered by Supabase promises to deliver an experience that is both seamless and powerful. With our platform, businesses can tackle their day-to-day operations with ease, making the most of their time and resources. Whether you're looking to manage financial transactions, view account balances, or optimize your financial performance, we've got you covered. So join us today and take your business to the next level!
               </div>
-              <div className="w-1/2 p-3 h-24">
-                <Link href="/business"><Button content="Are you a business?" /></Link>
+              <div className="flex items-center justify-center">
+                <div className="w-1/2 p-3 h-24">
+                  <Button content="Are you a customer?"/>
+                </div>
+                <div className="w-1/2 p-3 h-24 flex items-center justify-center" onClick={()=>{
+                    Cookies.set('type', 'business', { expires: 7 });
+                    signIn()
+                  }}>
+                  <Button>Are you a business?</Button>
+                </div>
               </div>
+            </Container>
+            {/* <button className="btn ">Image</button> */}
+          </div>
+            <div className="flex grow w-1/2">
+              <Image src={illustration} alt="photo" style ={{objectFit:"contain"}} className=""/>
             </div>
-          </Container>
-          {/* <Image src={illustration} alt="photo"/> */}
-          <button className="btn ">Image</button>
         </div>
       </div>
     </>
